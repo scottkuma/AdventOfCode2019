288128326aa201792c27b787c4390c39128b1adf
Scott Pettigrew <scottkuma@Scotts-MacBook-Pro.local>
2019-12-01 19:59:47 -0500

    Optimized code a bit.

--- Day1/day1-1.py
+++ Day1/day1-1.py
@@ -1,11 +1,10 @@
-filename = 'day1-1.txt'
-
 def fuel(mass):
-    return(int(mass / 3) - 2)
+    return(int(mass) // 3 - 2)
 
-with open(filename,'r') as infile:
-    sum = 0
-    for l in infile:
-        sum += fuel(int(l))
+data = open('day1-1.txt').readlines()
 
-print(sum) 
+sum = 0
+for mass in data:
+    sum += fuel(int(mass))
+
+print(sum)
--- Day1/day1-2.py
+++ Day1/day1-2.py
@@ -1,18 +1,17 @@
-filename = 'day1-1.txt'
-
 def fuel_rec(mass):
     print(f"m:{mass}")
-    fuel_mass = int(mass / 3) - 2
+    fuel_mass = int(mass) // 3 - 2
     if fuel_mass > 0:
         return fuel_mass + fuel_rec(fuel_mass)
     else:
         return 0
+
 sum = 0
-with open(filename,'r') as infile:
+data = open("day1-1.txt").readlines()
     #print(fuel_rec(14))
     #print(fuel_rec(1969))
     #print(fuel_rec(100756))
-    for l in infile:
-        sum += fuel_rec(int(l))
+for mass in data:
+    sum += fuel_rec(int(mass))
 
 print(sum)
