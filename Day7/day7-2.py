36b15d85f53d6c1e1b04dda1feba221958247b62
Scott Pettigrew <scottkuma@Scotts-MacBook-Pro.local>
2019-12-08 15:37:01 -0500

    Finished Day 7!

--- Day7/day7-2.py
+++ Day7/day7-2.py
@@ -1,25 +1,38 @@
 import sys
 from itertools import permutations
 
-from intcode import IntCode
+from intcode2 import IntCode
 
-filename = "day7_test2_data.txt"
+filename = "day7_data.txt"
 print ("Please input phase sequence:")
 phase_input = input().strip()
 max = 0
 max_perm = None
+for phase in permutations(phase_input,5):
+    #phase = phase_input
+    print(f"Trying Phase: {phase}")
+    a = IntCode(filename, "A", False)
+    b = IntCode(filename, "B", False)
+    c = IntCode(filename, "C", False)
+    d = IntCode(filename, "D", False)
+    e = IntCode(filename, "E", False)
 
-phase = phase_input()
+    a.process([phase[0]])
+    b.process([phase[1]])
+    c.process([phase[2]])
+    d.process([phase[3]])
+    e.process([phase[4]])
 
-print(f"Trying Phase: {phase}")
-a = IntCode(filename, True)
-b = IntCode(filename)
-c = IntCode(filename)
-d = IntCode(filename)
-e = IntCode(filename)
+    while e.state != "STOPPED":
+        a.process([e.output])
+        b.process([a.output])
+        c.process([b.output])
+        d.process([c.output])
+        e.process([d.output])
 
-a.process([phase[0]],0)
-#b.process([phase[1], a.output])
-#c.process([phase[2], b.output])
-#d.process([phase[3], c.output])
-#e.process([phase[4], d.output])
+    if int(e.output) > max:
+        print(f"NEW MAX! {e.output}")
+        max = int(e.output)
+        max_perm = phase
+
+print(max, max_perm)
